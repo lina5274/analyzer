2c9142154d3109239c793b9ece967efc745e4c31
lina5274 <167440004+lina5274@users.noreply.github.com>
2024-05-23 13:16:28 +0300

    Update ptoject.py

--- ptoject.py
+++ ptoject.py
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     path = './'
     pm = PriceMachine()
     pm.load_prices(path)
-    print("Для поска товара напишите его название. Если поиск окончен напишите 'exit'.")
+    print("Для поска товара введите его название. Если поиск окончен введите 'exit'.")
     while True:
         input_text = input('Введите текст для поиска: ')
         if input_text != 'exit':
